8458b7d67dffbc5c5e22f6db53c1a72b3d344a76
Daniel Marques <daniel.brz2009@gmail.com>
2021-06-10 00:42:48 -0300

    Scaffold and routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
+import { bootcampsRoutes } from './bootcamps.routes';
 import { contactRoutes } from './contact.routes';
+import { filesRoutes } from './files.routes';
+import { studentsRoutes } from './students.routes';
 import { usersRoutes } from './users.routes';
 
 const router = Router();
 
 router.use('/users', usersRoutes);
+router.use('/files', filesRoutes);
+router.use('/bootcamps', bootcampsRoutes);
+router.use('/students', studentsRoutes);
 router.use('/contact', contactRoutes);
 
 export { router };
---
+++ src/shared/infra/http/routes/students.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const studentsRoutes = Router();
+
+export { studentsRoutes };
